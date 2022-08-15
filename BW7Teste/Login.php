912e15d4f2ae33ae49ff86eb8387a99b3e95ed86
tekiann <leonardotekian@gmail.com>
2022-08-15 17:33:59 +0000

    Template

---
+++ BW7Teste/Login.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="BW7" content="Gerenciamento de veículos" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.1.0/lux/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>BW7 - Login</title>
+</head>
+
+<body class="container-fluid vh-100 p-0 my-auto">
+
+    <div class="p-3 h-100">
+
+
+        <div class="row d-flex h-100 justify-content-center align-content-center">
+
+        <form action="/login" method="post" class=" row col-12 col-md-8 mx-md-0 ">
+       
+            <h3 class="text-start">Login</h3>
+        
+            <div class="form-group input-group-sm col-12">
+                <label for="inputEmail">Email:</label>
+                <input type="email" class="form-control border-1 border-primary " name="email" id="inputEmail"
+                    aria-describedby="helpEmail" placeholder="Ex: email@email.com" value="{{ old('email') }}">
+                <small id="helpEmail" class="form-text text-muted">Seu email.</small>
+                <!--
+
+                    <span class="invalid-feedback is-invalid" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                -->
+            </div>
+        
+            <div class="form-group input-group-sm col-12">
+                <label for="inputPassword">Senha:</label>
+                <input type="password" class="form-control border-1 border-primary" name="password"
+                    id="inputPassword" aria-describedby="helpPassword" >
+                <small id="helpPassword" class="form-text text-muted">Sua senha</small>
+                <!--
+
+                    <span class="invalid-feedback is-invalid" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                -->
+                
+            </div>
+            <div class="d-grid gap-2 col-12 mt-3">
+                <button type="submit" class="btn btn-primary" type="button">Entrar</button>
+            </div>
+            <a href="/Auth/Register">Não Possui uma conta? Cadastre-se agora!</a>
+        </form>
+
+        </div>
+
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/js/bootstrap.min.js"></script>
+</body>
+
+</html>
+<?php
+?>
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
+Como política da nossa empresa, todo candidato a uma vaga precisa realizar um teste prático de conhecimento. Segue instruções do teste abaixo:
 
+Criar um sistema simples para gerenciamento de aluguel de carros
+•	Precisa ter acesso restrito por login e senha;
+•	Precisa ter um cadastro dos veículos;
+•	Precisa ter uma lista com os veículos cadastrados e nessa lista uma indicação se o veículo está alugado ou disponível;
+•	Nessa lista de veículos devem ser possíveis editar o cadastro de um veículo, excluir o veículo e marcar se o veículo foi alugado;
+•	Quando o veículo alugado for devolvido, tem que ser possível marcar o veículo como disponível novamente;
+
+Informações adicionais
+•	Você deve criar o banco de dados e as tabelas necessárias para esse sistema;
+•	Nos informe a versão do PHP utilizada na programação;
+•	O layout não será levado em consideração, entretanto, sugerimos a utilização do Bootstrap;
+•	O código, se for feito orientado a objetos, será considerado um diferencial;
+
+Envie seu código e banco de dados para o e-mail suporte@bw7.com.br até o dia 18/08/2021 às 18h.
+
+O não envio do código acima, será entendido que você desistiu de concorrer a vaga.
+
+Boa sorte!
